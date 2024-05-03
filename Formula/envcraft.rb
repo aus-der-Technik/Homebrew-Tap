041beca49a5b88913d430af1cb26d33c2aa58673
Kris Simon <kris@ausdertechnik.de>
2024-05-03 13:23:03 +0200

    Update envcraft to 1.0.1

--- Formula/envcraft.rb
+++ Formula/envcraft.rb
@@ -4,8 +4,8 @@
 class Envcraft < Formula
   desc "EnvCraft is an .env file creator. You feed it with a given env.sample file and EnvCraft will create a corresponding .env file while asking you for all mandatory values. EnvCraft supports defining default values (press return to accept) and custom prompts."
   homepage "https://ausdertechnik.de"
-  url "https://github.com/aus-der-Technik/EnvCraft/releases/download/1.0.0/envcraft"
-  sha256 "b6daf3eab8864cc39019f2c48d4a6280863a3e0fd1a0dce0a351da7367f8c7a8"
+  url "https://github.com/aus-der-Technik/EnvCraft/releases/download/1.0.1/envcraft"
+  sha256 "b1a47c2dd6f827dd8c075a2ffe2f71c430799564d6da004dff3f27fea767413c"
   license "BSD-2-Clause"
 
   def install
